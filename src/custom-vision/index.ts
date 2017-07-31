9cf11aa8731a3f4d3537745cb6a19126d35c5365
Tal Ben-Ari <t-tabena@microsoft.com>
2017-07-31 12:40:25 -0700

    Start work on converting training to class

--- src/custom-vision/index.ts
+++ src/custom-vision/index.ts
@@ -3,6 +3,7 @@ import { genBaseURL } from '../utils'
 import { region, service, version } from './constants'
 import { CustomVisionPredictor } from './predictor'
 import * as types from './types'
+import { Account, Config, Domain } from './types'
 
 export { types }
 
@@ -20,10 +21,13 @@ export class CustomVision {
   /** The active project. */
   private predictor?: CustomVisionPredictor
 
-  /** The account information */
-  private accountInfo?: types.Account
+  /** The account information. */
+  private accountInfo?: Account
 
-  constructor({ trainingKey, predictionKey, projectID }: types.Config) {
+  /** All available domains in Custom Vision. */
+  private domains?: Domain[]
+
+  constructor({ trainingKey, predictionKey, projectID }: Config) {
     this.trainingKey = trainingKey
     if (predictionKey) this.predictionKey = predictionKey
 
@@ -38,19 +42,59 @@ export class CustomVision {
     }
   }
 
+  /** The predict method, pulled from the predictor for easier access. */
+  get predict() {
+    if (!this.predictor) {
+      throw new Error('No project ID provided')
+    }
+
+    return this.predictor.predict
+  }
+
   /**
    * Get the account's information.
    * 
    * @returns the account info.
    */
-  async getAccountInfo(): Promise<types.Account> {
+  async getAccountInfo(): Promise<Account> {
     if (!this.accountInfo) {
       this.accountInfo = (await get(CustomVision.baseURL + '/account').set(
         'Training-key',
         this.trainingKey
-      )).body as types.Account
+      )).body as Account
     }
 
     return this.accountInfo
+  }
+
+  /**
+   * Get a list of all available domains.
+   * 
+   * @param force whether or not to force the function to re-fetch the info, disabling memoization.
+   * @returns the list of domains.
+   */
+  async getDomains(force = false): Promise<Domain[]> {
+    if (force || !this.domains) {
+      this.domains = (await get(CustomVision.baseURL + '/domains').set(
+        'Training-Key',
+        this.trainingKey
+      )).body as Domain[]
+    }
+
+    return this.domains
+  }
+
+  /**
+   * Get a domain object given its name.
+   *
+   * @param name the domain name.
+   * @returns the domain's details.
+   */
+  async findDomain(name: string): Promise<Domain> {
+    const domain = (await this.getDomains()).find(
+      (el: Domain) => el.Name === name
+    )
+    if (!domain) throw new Error(`Domain not found: ${name}`)
+    return domain
   }
 }
