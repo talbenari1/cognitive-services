a2525f04f3884fee3479a50dfbb8f39697326186
Tal Ben-Ari <t-tabena@microsoft.com>
2017-07-31 17:24:38 -0700

    Convert predictor to use genURL method

--- src/custom-vision/predictor.ts
+++ src/custom-vision/predictor.ts
@@ -39,16 +39,26 @@ export class CustomVisionPredictor {
     const binary = image.constructor !== String
 
     // create the correct endpoint
-    let endpoint = binary ? '/image' : '/url'
-    if (save) endpoint = '/inline' + endpoint
+    let endpoint = binary ? 'image' : 'url'
+    if (save) endpoint = 'inline/' + endpoint
 
     // build the request
-    let req = post(CustomVisionPredictor.baseURL + endpoint).set(
+    let req = post(this.genURL(endpoint)).set(
       'Prediction-Key',
       this.predictionKey
     )
     if (iterationID) req = req.query({ iterationId: iterationID })
 
     return (await req.send(binary ? image : { Url: image })).body
+  }
+
+  /** 
+   * Generate an API URL given an endpoint and an optional iteration ID.
+   *
+   * @param endpoint the API endpoint to connect to. 
+   * @returns the generated URL. 
+   */
+  private genURL(endpoint: string) {
+    return `${CustomVisionPredictor.baseURL}/${this.projectID}/${endpoint}`
   }
 }
