915fac1a72636a6ebdd2493ef5b52d3a150762d2
Tal Ben-Ari <t-tabena@microsoft.com>
2017-08-07 16:14:46 -0700

    Remove test file

--- src/test.ts
+++
@@ -1,8 +0,0 @@
-import { CustomVision } from '.'
-
-const config = {
-  trainingKey: '2db255846d014d05917cd9c66a6816c8',
-  projectID: 'efe50762-4c60-41be-af86-d6464ce46984'
-}
-
-const customVision = new CustomVision(config)
